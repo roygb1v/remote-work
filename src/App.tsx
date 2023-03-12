e6b31bde02f183fd54371be4b749a20bb32cf42d
Roy <cjhroy@outlook.com>
2023-03-13 00:10:56 +0800

    added more companies and an alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Table, Badge } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
-import { AutoComplete, Input } from 'antd';
+import { AutoComplete, Input, Alert } from 'antd';
 import { MoreOutlined } from '@ant-design/icons';
 import dataArray from "../db.json"
 import './index.css'
@@ -18,6 +18,7 @@ const columns: ColumnsType<DataType> = [
   {
     title: 'Company',
     width: 120,
+    sorter: true,
     dataIndex: 'company',
     key: 'company',
     fixed: 'left',
@@ -35,10 +36,11 @@ const columns: ColumnsType<DataType> = [
   {
     title: 'Policy',
     width: 100,
+    sorter: true,
     dataIndex: 'policy',
     key: 'policy',
   },
-  { title: 'Notes', dataIndex: 'notes', key: 'notes' },
+  { title: 'Notes', sorter: true, dataIndex: 'notes', key: 'notes' },
 ];
 
 const getData = () => {
@@ -56,7 +58,6 @@ const getData = () => {
 }
 
 const App: React.FC = () => <HomePage />
-
 
 const HomePage = () => {
   const d = getData()
@@ -80,9 +81,14 @@ const HomePage = () => {
   }
 
   return <div>
+    <Alert
+      description={`We provide the latest updates in wfh policy for tech companies. If the information provided here is lacking or inaccurate, please send an email to toplisthello@gmail.com`}
+      type="success"
+    />
+
     <AutoComplete
       dropdownMatchSelectWidth={252}
-      style={{ width: window.innerWidth - 16, margin: '0 0 1rem 0' }}
+      style={{ width: window.innerWidth - 16, margin: '1rem 0 1rem 0' }}
       onSearch={handleSearch}
     >
       <Input.Search size="large" placeholder="Search company name" onKeyDown={handleBackspace} enterButton />
@@ -93,6 +99,7 @@ const HomePage = () => {
       rowClassName={(_, index) => index % 2 === 0 ? 'table-row-light' : 'table-row-dark'}
       dataSource={data}
       scroll={{ x: window.innerWidth + 100, y: window.innerHeight }}
+      pagination={{ defaultCurrent: 1, showSizeChanger: true, pageSizeOptions: ['10', '20'] }}
     />
   </div>
 }
